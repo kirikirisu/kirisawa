0563f3be494088a4ce4acaa798a7e06cf8d8b376
kirikirisu <b1801815@planet.kanazawa-it.ac.jp>
2023-07-01 17:58:44 +0900

    add hlci server

--- lighthouserc.cjs
+++ lighthouserc.cjs
@@ -1,7 +1,10 @@
 module.exports = {
   ci: {
     upload: {
-      target: "temporary-public-storage",
+      target: "lhci",
+      serverBaseUrl: "http://18.182.38.106:9001",
+      // https://github.com/GoogleChrome/lighthouse-ci/blob/main/docs/getting-started.md#configuration
+      token: "5592b40d-37b2-49c0-8b12-7638818f6341",
     },
   },
 };
